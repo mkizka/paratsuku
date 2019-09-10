a1ef52c8dd2585f6bcf4a49b7992d08ee8fd4d67
Compeito <com0806peito@icloud.com>
2019-09-10 22:20:57 +0900

    manifest.json整備

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,30 @@
+const packageJson = require('./package')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   configureWebpack: {
     plugins: [
-      new FaviconsWebpackPlugin('./src/assets/profile.png')
+      new FaviconsWebpackPlugin({
+        logo: './src/assets/profile.png',
+        favicons: {
+          appName: 'パラツク -Paratsuku- パラパラ漫画を書いて即ツイート',
+          appShortName: 'Paratsuku',
+          description: packageJson.description,
+          version: packageJson.version,
+          lang: 'ja',
+          theme_color: '#363636',
+          icons: {
+            android: true,
+            appleIcon: true,
+            appleStartup: true,
+            coast: false,
+            favicons: true,
+            firefox: false,
+            windows: false,
+            yandex: false
+          }
+        }
+      })
     ]
   }
 }
